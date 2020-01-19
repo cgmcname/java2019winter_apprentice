5f111b48b64c6e4086ae7617a5e39898adfafade
chrismcnames <chrismcnames@192.168.0.118>
2020-01-19 15:26:59 -0600

    clean up

--- EmployeeProject/src/EmployeeMain.java
+++ EmployeeProject/src/EmployeeMain.java
@@ -1,28 +1,23 @@
 
-
-
 public class EmployeeMain {
 
 	public static void main(String[] args) {
-		
-		//test for git 
-		//Try from here
-		
-		Employee emp1 = new Employee(1,"1","David", 'B', "Banner", "1/1/1960", 100000.00);
-		
-		Employee emp2 = new Employee(2,"2","Thor", "Odinson", "1/1/1660", 10.00);
-		
-		Employee emp3 = new Employee(3,"3","John", 'J', "Doe", "1/1/1340");
-		
+
+		Employee emp1 = new Employee(1, "1", "David", 'B', "Banner", "1/1/1960", 100000.00);
+
+		Employee emp2 = new Employee(2, "2", "Thor", "Odinson", "1/1/1660", 10.00);
+
+		Employee emp3 = new Employee(3, "3", "John", 'J', "Doe", "1/1/1340");
+
 		System.out.println(emp1);
-		
+
 		System.out.println();
 		System.out.println();
-		
+
 		System.out.println(emp1.getDateOfBirth());
-		
+
 		System.out.println(emp2.getMiddleIntial());
-		
+
 		System.out.println(emp3.getSalary());
 
 	}
